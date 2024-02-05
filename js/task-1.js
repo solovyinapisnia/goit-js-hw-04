a126967a86058766a9e07d802014fcb85afd44c6
Anastasiia <145344061+solovyinapisnia@users.noreply.github.com>
2024-02-05 23:29:00 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function slugify(title) {
+    title = title.toLowerCase()
+    let arrayOfWords = title.split(" ")
+let slug = arrayOfWords.join("-")
+    return slug
+}
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
